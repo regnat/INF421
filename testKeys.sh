ebdae8f1b05247d741e6a6696ea92928b3e35e93
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.edu>
2015-01-05 21:13:58 +0100

    Restored the right crypted message in testKeys.sh

--- testKeys.sh
+++ testKeys.sh
@@ -7,5 +7,5 @@ do
     openssl asn1parse -genconf $FILE -out ${FILE}.der
     openssl rsa -in ${FILE}.der -inform der -text -check
     openssl rsa -inform der -outform pem -in ${FILE}.der -out ${FILE}.pem
-    openssl rsautl -inkey ${FILE}.pem -decrypt -in testKey0.txt -out decrypted.${num}.txt
+    openssl rsautl -inkey ${FILE}.pem -decrypt -in ../files/bigsecret.txt -out decrypted.${num}.txt
 done
